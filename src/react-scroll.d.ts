9db94ad086be0c0e2e1bea99fa20c304c158b2c4
Aayan Haque <mdaayanhaque8114@gmail.com>
2025-05-23 19:27:18 +0530

    Updates

--- src/react-scroll.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-scroll' {
-  export const Link: any;
-  export const Element: any;
-  export const Events: any;
-  export const scroll: any;
-  export const scrollSpy: any;
-  export const scroller: any;
-} 
